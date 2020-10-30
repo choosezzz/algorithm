6a7d71e3d8d5f8c75d8e9c6a5b95a65a0d5e4b4f
dingshuangen <dingshuangen@58.com>
2020-10-30 22:15:32 +0800

    剑指offer21题，压栈出栈

--- src/main/java/newcoder/Offer021.java
+++ src/main/java/newcoder/Offer021.java
@@ -1,9 +1,11 @@
 package main.java.newcoder;
 
+import java.util.Stack;
+
 /**
  * @author dingshuangen
  * @date 2020/5/11 1:06
- *
+ * <p>
  * 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。
  * 假设压入栈的所有数字均不相等。
  * 例如序列1,2,3,4,5是某栈的压入顺序，
@@ -13,9 +15,34 @@ package main.java.newcoder;
  */
 public class Offer021 {
 
-    public boolean IsPopOrder(int [] pushA,int [] popA) {
+    /**
+     * 运行时间：10ms
+     * <p>
+     * 占用内存：9396k
+     *
+     * 借助栈来模拟实际的操作顺序
+     * @param pushA
+     * @param popA
+     * @return
+     */
+    public boolean IsPopOrder(int[] pushA, int[] popA) {
 
+        if (popA == null || popA == null
+                || popA.length <= 0 || pushA.length <= 0
+                || popA.length != pushA.length) {
+            return false;
+        }
 
-        return false;
+        Stack<Integer> stack = new Stack<>();
+        int j = 0;
+        for (int i = 0; i < pushA.length; i++) {
+            stack.push(pushA[i]);
+            //判断当前的栈顶元素是否在弹出序列里
+            while (!stack.isEmpty() && stack.peek() == popA[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
     }
 }
