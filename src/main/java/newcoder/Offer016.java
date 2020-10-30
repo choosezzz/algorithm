d75dc8d28f75f1e9bd5dbe4e6450040a58b13d0d
dingshuangen <dingshuangen@58.com>
2020-10-30 17:28:34 +0800

    复习两个链表合并

--- src/main/java/newcoder/Offer016.java
+++ src/main/java/newcoder/Offer016.java
@@ -46,6 +46,7 @@ public class Offer016 {
     /**
      * 运行时间：30ms
      * 占用内存：9680k
+     *
      * @param list1
      * @param list2
      * @return
@@ -67,7 +68,7 @@ public class Offer016 {
 
                 if (head == null) {
                     head = current = list1;
-                }else {
+                } else {
                     //当前节点永远指向以merge链表的尾部
                     current.next = list1;
                     current = current.next;
@@ -76,7 +77,7 @@ public class Offer016 {
             } else {
                 if (head == null) {
                     head = current = list2;
-                }else {
+                } else {
                     current.next = list2;
                     current = current.next;
                 }
@@ -90,6 +91,46 @@ public class Offer016 {
         //list2全部merge完
         if (list2 == null) {
             current.next = list1;
+        }
+        return head;
+    }
+
+    /**
+     * 每次比较两个链表的头节点，值小的节点向后移动
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public ListNode mergeList(ListNode list1, ListNode list2) {
+
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode head = null;
+        ListNode curr = null;
+        while (list1 != null && list2 != null){
+
+            if (list1.val <= list2.val){
+
+                if (head == null){
+                    head = curr = list1;
+                }else {
+                    curr.next = list1;
+                }
+                //链表向后移动
+                list1 = list1.next;
+            }else {
+                if (head == null){
+                    head = curr = list2;
+                }else {
+                    curr.next = list2;
+                }
+                list2 = list2.next;
+            }
         }
         return head;
     }
