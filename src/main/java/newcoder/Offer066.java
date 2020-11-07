67131056ef27316cd52f094bca9878799786ee4d
dingshuangen <dingshuangen@58.com>
2020-11-08 00:06:41 +0800

    机器人运动范围

--- src/main/java/newcoder/Offer066.java
+++ src/main/java/newcoder/Offer066.java
@@ -26,15 +26,35 @@ public class Offer066 {
         if (threshold < 0 || rows < 0 || cols < 0) {
             return 0;
         }
+        //记录每个格子是否已走过
         int[][] flag = new int[rows][cols];
         return dfs(0, 0, threshold, rows - 1, cols - 1, flag);
     }
 
+    /**
+     * 深度优先遍历
+     * @param row 当前行
+     * @param col 当前列
+     * @param threshold 阈值
+     * @param rows 总行数
+     * @param cols 总列数
+     * @param flag 格子使用标记
+     * @return
+     */
     private int dfs(int row, int col, int threshold, int rows, int cols, int[][] flag) {
+
+        /*
+        条件判断：
+        1.是否超出格子范围
+        2.当前格子是否已被走过
+        3.是否超过阈值
+         */
         if (row > rows || row < 0 || col > cols || col < 0 || flag[row][col] == 1 || sum(row, col) > threshold) {
             return 0;
         }
+        //当前格子满足条件，则标记为已使用
         flag[row][col] = 1;
+        //次数加一，向上下左右分别运动
         return 1 + dfs(row - 1, col, threshold, rows, cols, flag)
                 + dfs(row + 1, col, threshold, rows, cols, flag)
                 + dfs(row, col - 1, threshold, rows, cols, flag)
