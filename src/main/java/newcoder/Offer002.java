f88688490f413987973fa65270fd49a94cf953d0
dingshuangen <dingshuangen@58.com>
2020-11-20 23:29:41 +0800

    空格替换为%20

--- src/main/java/newcoder/Offer002.java
+++ src/main/java/newcoder/Offer002.java
@@ -25,6 +25,34 @@ public class Offer002 {
         }
         return new String(result, 0, size);
     }
+    public String replaceSpace(StringBuffer str) {
+
+        if(str == null || str.length() <= 0){
+            return "";
+        }
+        int p1 = str.length() - 1;
+        for (int i = 0; i <= p1; i++) {
+            if (str.charAt(i) == ' '){
+                //两个空格
+                str.append("  ");
+            }
+        }
+        int p2 = str.length() - 1;
+        while (p1 >= 0 && p2 > p1){
+            char c = str.charAt(p1--);
+            if (c == ' '){
+                str.setCharAt(p2--, '0');
+                str.setCharAt(p2--, '2');
+                str.setCharAt(p2--, '%');
+
+            }else {
+                str.setCharAt(p2--, c);
+            }
+        }
+        return str.toString();
+
+    }
+
 
     public static void main(String[] args) {
         System.out.println(repalceSpace("We Are Happy."));
